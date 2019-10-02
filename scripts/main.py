4d0bf3f18206851025d6ecebfb093cb6710736af
Hair Parra <jair.parra@outlook.com>
2019-10-02 16:47:59 -0400

    Add files via upload

---
+++ scripts/main.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 16:26:34 2019
+
+@ COMP 551: Applied Machine Learning (Winter 2019) 
+@ Mini-project 1 : Implementing Logistic Regression and LDA from scratch 
+# Team Members: 
+
+@ Hair Albeiro Parra Barrera 
+@ ID: 260738619 
+
+@ Ashray Mallesh
+@ ID: 260838256
+    
+@ Hamza Khan
+@ ID: 
+"""
+
+# *****************************************************************************
+
+### *** Reddit Multi-output classification *** #### 
+
+# *****************************************************************************
+
+#### 1. Imports ### 
+
+import re 
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from tqdm import tqdm # to display progress bar
+
+# *****************************************************************************
+
+### 2. Reading and cleaning the data ### 
+
+
+
+
+
+
+
+
